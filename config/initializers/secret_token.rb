4725ffe3148a536f4e152668bf3c2d12d02ce863
Alex Popelard <apopelard2014@kellogg.northwestern.edu>
2013-11-10 22:56:57 -0600

    Created Rollcall App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rollcall::Application.config.secret_key_base = 'fdb519b721ea26ab02b7ce1190a07939ead39088e35516721dd68bd3dc8e6a9c429087b61cf2dd02e329ddd93550e213992d74a14d805161c375fb5c3197bee6'
